c71ecfb1b42a6011e9c339be2190215bfa198f92
blingyplus <blingyplusinc@gmail.com>
2024-12-11 16:14:41 +0000

    update L5 Swagger configuration

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -91,7 +91,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_BASE_PATH', null),
+            'base' => env('L5_SWAGGER_BASE_PATH', 'https://calm-sparkle-production.up.railway.app'),
 
             /*
              * Absolute path to directories that should be excluded from scanning
@@ -312,7 +312,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://calm-sparkle-production.up.railway.app'),
         ],
     ],
 ];
